29cf0e394ac89356dd572af17c25aea4d60e93c3
padmajapinnika <padmajapinnika@gmail.com>
2025-04-18 15:09:36 -0400

    integarting feature3 scenario2

--- src/components/NumberOfEvents.jsx
+++ src/components/NumberOfEvents.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 const NumberOfEvents = ({ currentNOE = 32, setCurrentNOE }) => {
-  const [number, setNumber] = useState(currentNOE);
+  const [number, setNumber] = useState(32);
 
   useEffect(() => {
     setNumber(currentNOE);
@@ -9,23 +9,28 @@ const NumberOfEvents = ({ currentNOE = 32, setCurrentNOE }) => {
 
   const handleInputChanged = (event) => {
     const value = parseInt(event.target.value);
-    setNumber(value);
-    if (setCurrentNOE) {
-      setCurrentNOE(value);
+    if (!isNaN(value)) {
+      setNumber(value);
+      if (setCurrentNOE) {
+        setCurrentNOE(value);
+      }
+    } else {
+      setNumber('');
     }
   };
-
+  
   return (
     <div id="number-of-events" data-testid="number-of-events">
       <label htmlFor="num-events-input">Number of events:</label>
+
       <input
         id="num-events-input"
         type="number"
-        value={number}
+        value={isNaN(number) ? '' : number}
         onChange={handleInputChanged}
-        min="1"
+        //min="1"
         role="textbox"
-        aria-label="Number of events"
+        //aria-label="Number of events"
       />
     </div>
   );
