180a0b589a3ca677c9f731cf4a04cecfb08096e9
padmajapinnika <padmajapinnika@gmail.com>
2025-04-22 15:47:02 -0400

    remoce atatus test error

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as atatus from 'atatus-spa';
+atatus.config('d7dd5897155640abb3be9c8721f2e187').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
