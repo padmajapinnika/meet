82b21c821e9e5b0f93004f5f892ee4bb670c6a14
padmajapinnika <padmajapinnika@gmail.com>
2025-04-24 11:13:11 -0400

    Enable service worker for PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
-atatus.config('d7dd5897155640abb3be9c8721f2e187').install();
+//import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+//atatus.config('d7dd5897155640abb3be9c8721f2e187').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
-
+);
+serviceWorkerRegistration.register();
 //atatus.notify(new Error('Test Atatus Setup'));
