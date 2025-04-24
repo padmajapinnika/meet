3d96b2cad664667722df00fe03d4251d5884cd66
padmajapinnika <padmajapinnika@gmail.com>
2025-04-24 13:34:02 -0400

    Add warning alert for offline users and fix fetch logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
-import { InfoAlert, ErrorAlert } from './components/Alert';
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
 
 
 
@@ -19,9 +19,15 @@ const App = () => {
  const [currentCity, setCurrentCity] = useState("See all cities");
  const [infoAlert, setInfoAlert] = useState("");
  const [errorText, setErrorText] = useState('');
+ const [warningAlert, setWarningAlert] = useState("");
 
 
  useEffect(() => {
+  if (navigator.onLine) {
+    setWarningAlert("");
+  } else {
+    setWarningAlert("You are offline. The displayed event list has been loaded from the cache.");
+  }
    fetchData();
  }, [currentCity, currentNOE]);
 
@@ -41,6 +47,7 @@ const App = () => {
      <div className="alerts-container">
        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
        {errorText.length > 0 && <ErrorAlert text={errorText} />}
+       {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
      </div>
     <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
     <NumberOfEvents currentNOE={currentNOE} setCurrentNOE={setCurrentNOE} setErrorText={setErrorText}/>
--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -48,4 +48,11 @@ class ErrorAlert extends Alert {
   }
 }
 
-export { InfoAlert, ErrorAlert };
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(255, 165, 0)';
+    this.bgColor = 'rgb(255, 213, 128)';
+  }
+ }
+ export { InfoAlert, ErrorAlert, WarningAlert };
