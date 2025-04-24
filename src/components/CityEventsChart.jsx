f5d2a0d64659b579d4d8b735b2d56e575ac68716
padmajapinnika <padmajapinnika@gmail.com>
2025-04-24 15:45:04 -0400

    made some chnages to axis

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -18,8 +18,8 @@ const CityEventsChart = ({ allLocations, events }) => {
 
   const getData = () => {
     const data = allLocations.map((location) => {
-      const count = events.filter(
-        (event) => event.location === location
+      const count = events
+      .filter( (event) => event.location === location
       ).length;
       const city = location.split(/, | - /)[0];
       return { city, count };
@@ -34,7 +34,7 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 20,
           right: 20,
-          bottom: 80,
+          bottom: 60,
           left: -30,
         }}
       >
