a13a372ae005322c3c2a77eead682071c97dfab1
padmajapinnika <padmajapinnika@gmail.com>
2025-04-25 15:30:35 -0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/meet/service-worker.js', { scope: '/meet/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/service-worker.js', { scope: '/' })})}
